d1a3c7d13950e6d7048ae6686b6287f715373bd3
ryo <>
2023-04-09 23:14:40 +0900

    firebase(cloud firestore)にデータを保存するところまで終了。次はhttps://www.youtube.com/watch?v=9NOg_HSbo9w の13:30から

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBxiNYisfXVelnFr-uGFD7Nlpoewr-pdCA",
+  authDomain: "react-firebase-tutorial-cde34.firebaseapp.com",
+  projectId: "react-firebase-tutorial-cde34",
+  storageBucket: "react-firebase-tutorial-cde34.appspot.com",
+  messagingSenderId: "35438797961",
+  appId: "1:35438797961:web:90d9a5f5c4b21b81439258",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
